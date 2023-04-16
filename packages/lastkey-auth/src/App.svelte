d9a8e381bbf0d48b2f1aad0cac29a1b1bdaa221c
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-04-16 14:11:07 +0800

    init auth module

---
+++ packages/lastkey-auth/src/App.svelte
@@ -0,0 +1,32 @@
+<svelte:options tag="lastkey-auth" />
+
+<script lang="ts">
+    import { nanoid } from 'nanoid';
+    import writeQR from '@paulmillr/qr';
+
+    const svgqr = writeQR(nanoid(64), 'svg', {
+        scale: 6,
+    });
+    const svg = svgqr.replaceAll('svg:rect', 'rect');
+</script>
+
+<main>
+    <figure>
+        {#if svg}
+            {@html svg}
+        {:else}
+            <svg />
+        {/if}
+        <figcaption>Scan this QR code with your LastKey app</figcaption>
+    </figure>
+</main>
+
+<style lang="scss">
+    @import './styles/global';
+
+    svg {
+        fill: var(--color);
+        width: 246px;
+        height: 246px;
+    }
+</style>
